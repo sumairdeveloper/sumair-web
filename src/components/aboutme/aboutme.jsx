308eca746217f93aa9b92696764aea639c8cd6dd
sumairdeveloper <sumairbughio70@gmail.com>
2025-01-21 18:59:15 +0500

    ...

--- src/components/aboutme/aboutme.jsx
+++ src/components/aboutme/aboutme.jsx
@@ -1,64 +1,93 @@
 import React from 'react'
-import './aboutme.css'
-import { FaLocationArrow, FaRegMoon } from 'react-icons/fa'
+import './AboutMe.css'
+import aboutMeImage from '../../assets/OIP.jpg'
+import Card from '../shared-component/shared-component'
+import { BiCodeAlt } from 'react-icons/bi'
+import { CgCodeSlash } from 'react-icons/cg'
+import { VscVscode } from 'react-icons/vsc'
+import { IoGitMerge, IoLogoFigma, IoLogoFirebase } from 'react-icons/io5'
+import { DiMongodb } from 'react-icons/di'
 
-const Header = () => {
+const AboutMe = () => {
 
-
-    const headerListItems = [
+    const cardData = [
         {
-            name: "Home",
-            route: "/"
+            cardheading: "Languages",
+            carddesc: "HTML5, CSS3, JS, REACTJS",
+            cardicon: <CgCodeSlash />
         },
         {
-            name: "About me",
-            route: "/aboutme"
+            cardheading: "Education",
+            carddesc: "BSCS from ABC UNI, Master",
+            cardicon: <CgCodeSlash />
         },
         {
-            name: "Services",
-            route: "/services"
-        },
-        {
-            name: "My Work",
-            route: "/mywork"
-        },
-        {
-            name: "Contact Me",
-            route: "/contactme"
-        },
+            cardheading: "Projects",
+            carddesc: "Build portfolio website.",
+            cardicon: <CgCodeSlash />
+        }
     ]
 
+    const toolsIcons = [
+        <VscVscode size={45} color='blue' />,
+        <IoLogoFirebase size={45} color='yellow' />,
+        <DiMongodb size={45} color='green' />,
+        <IoLogoFigma size={45} color='lightblue' />,
+        <IoGitMerge size={45} color='red' />
 
+    ]
     return (
-        <header className='header'>
-
-            <div className='header-logo'>
-                <h1>Tauseeq</h1>
-                <span className='name-dot'></span>
+        <div className='about-me'>
+            <div className='about-me-top'>
+                <span>Introduction</span>
+                <h1>About Me</h1>
             </div>
 
 
-
-            <ul className='header-list-items'>
-                {headerListItems.map((item, index) => {
-                    return (
-                        <li key={index}>{item.name}</li>
-                    )
-                })}
-            </ul>
+            <div className='about-me-bottom'>
+                <div className='about-me-bottom-image'>
+                    <img src={aboutMeImage} className='about-me-image' />
+                </div>
 
 
-            <div className='header-buttons'>
-                <FaRegMoon size={28 } />
-                <button className='contact-button'>
-                    Contact
-                    <FaLocationArrow />
-                </button>
+                <div className='about-me-bottom-right'>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Omnis rem enim blanditiis deleniti ipsam molestiae quos ut ratione aliquam! Accusamus, totam dignissimos maxime obcaecati ipsum non sit perspiciatis eligendi? Minima!
+                    </p>
+
+                    <div className='about-me-bottom-cards'>
+                        {cardData.map((item, index) => {
+                            return (
+
+                                <Card
+                                    cardHeading={item.cardheading}
+                                    cardDescription={item.carddesc}
+                                    cardIcon={item.cardicon}
+                                    className="card"
+                                />
+
+                            )
+                        })}
+                    </div>
+
+
+
+                    <span>Toos I use</span>
+
+                    <div className='about-me-bottom-tools-container'>
+                        {toolsIcons.map((item, index) =>
+
+                            <span className='about-me-bottom-tools'>
+                                {item}
+                            </span>
+
+                        )}
+                    </div>
+
+
+                </div>
             </div>
-
-
-        </header>
+        </div>
     )
 }
 
-export default Header
+export default AboutMe
